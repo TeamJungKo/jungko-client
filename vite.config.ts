ad676cc5ee20f701ba0e72f5ac97a550ffb1ffa9
sichoi42 <42.4.sichoi@gmail.com>
2023-11-16 13:51:49 +0900

    [FE] FEAT: reverse proxy를 위한 vite 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+      clientPort: 5173,
+      host: 'localhost',
+      path: '/hmr'
+    }
+  }
+});
