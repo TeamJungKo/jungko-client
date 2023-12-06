3357d7fcd29f886d65eb325a1cc8ba93f33ec970
pero <leesanghoon1@naver.com>
2023-12-06 17:32:03 +0900

    [FE] FIX : 카테고리 검색 기능 오류 수정

--- src/components/common/SearchResultComponent.tsx
+++ src/components/common/SearchResultComponent.tsx
@@ -279,6 +279,7 @@ const SearchResultComponent: React.FC<SearchResultComponentProps> = ({
       <CreateCardPage
         open={isCreateCardOpen}
         handleClose={handleCloseCreateCard}
+        searchOption={SearchOption}
       />
     </>
   );
